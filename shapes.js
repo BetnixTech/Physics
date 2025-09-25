1c34514aaa99df8c59eaf9851de5e421811ac389
Betnix CEO <gstar3181@gmail.com>
2025-09-25 17:59:13 -0400

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -85,3 +85,110 @@ class Shape {
     }
   }
 }
+
+// shapes.js
+
+export const shapes = [];
+
+export class Shape {
+    constructor(x, y, type = "circle", size = 50, color = "white", mass = 1) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.size = size;
+        this.color = color;
+        this.mass = mass;
+
+        // velocity
+        this.vx = 0;
+        this.vy = 0;
+
+        // rotation
+        this.rotation = 0; // radians
+        this.angularVelocity = 0; // radians per frame
+    }
+
+    draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x + this.size / 2, this.y + this.size / 2);
+        ctx.rotate(this.rotation);
+        ctx.fillStyle = this.color;
+
+        switch (this.type) {
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(0, 0, this.size / 2, 0, Math.PI * 2);
+                ctx.fill();
+                break;
+
+            case "square":
+                ctx.fillRect(-this.size / 2, -this.size / 2, this.size, this.size);
+                break;
+
+            case "triangle":
+                ctx.beginPath();
+                ctx.moveTo(0, -this.size / 2);
+                ctx.lineTo(this.size / 2, this.size / 2);
+                ctx.lineTo(-this.size / 2, this.size / 2);
+                ctx.closePath();
+                ctx.fill();
+                break;
+
+            case "polygon":
+                const sides = 5; // default pentagon
+                ctx.beginPath();
+                for (let i = 0; i < sides; i++) {
+                    const angle = (i / sides) * 2 * Math.PI;
+                    const px = Math.cos(angle) * this.size / 2;
+                    const py = Math.sin(angle) * this.size / 2;
+                    if (i === 0) ctx.moveTo(px, py);
+                    else ctx.lineTo(px, py);
+                }
+                ctx.closePath();
+                ctx.fill();
+                break;
+
+            case "line":
+                ctx.beginPath();
+                ctx.moveTo(-this.size / 2, 0);
+                ctx.lineTo(this.size / 2, 0);
+                ctx.lineWidth = 3;
+                ctx.strokeStyle = this.color;
+                ctx.stroke();
+                break;
+        }
+
+        ctx.restore();
+    }
+}
+
+// Factory functions for convenience
+export function createCircle(x, y, size = 50, color = "white", mass = 1) {
+    const c = new Shape(x, y, "circle", size, color, mass);
+    shapes.push(c);
+    return c;
+}
+
+export function createSquare(x, y, size = 50, color = "white", mass = 1) {
+    const s = new Shape(x, y, "square", size, color, mass);
+    shapes.push(s);
+    return s;
+}
+
+export function createTriangle(x, y, size = 50, color = "white", mass = 1) {
+    const t = new Shape(x, y, "triangle", size, color, mass);
+    shapes.push(t);
+    return t;
+}
+
+export function createPolygon(x, y, size = 50, color = "white", mass = 1) {
+    const p = new Shape(x, y, "polygon", size, color, mass);
+    shapes.push(p);
+    return p;
+}
+
+export function createLine(x, y, size = 50, color = "white", mass = 1) {
+    const l = new Shape(x, y, "line", size, color, mass);
+    shapes.push(l);
+    return l;
+}
